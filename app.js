eadb27b66c6377b930e84b319ff061b8e002129e
Hackathon User <kushwaha@local.dev>
2026-09-20 23:14:44 +0530

    PATCH  /assignments/:id

--- app.js
+++ app.js
@@ -40,6 +40,30 @@ app.get('/assignments', async (req, res) => {
     }
 });
 
+app.patch('/assignments/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        const result = await pool.query(
+            `UPDATE assignments
+            SET submitted = true
+            WHERE id = $1
+            RETURNING *;`,
+            [id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message: 'Assignments Not present'
+            });
+        };
+        res.status(200).json(result.rows[0]);
+    }catch(err){
+    console.log(err.message);
+    res.status(502).json({
+        errorMessage: 'Server is not running'
+    });
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
